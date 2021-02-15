b057926dd76b6ddfd84362ee1d763496b6e5625c
i-adarsh <i.adarsh.kr@icloud.com>
2021-02-15 07:20:41 +0530

    Uploaded from bash

---
+++ C++/Basics/Vowel.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+	char ch;
+	cout << "Enter an Alphabet : ";
+	cin >> ch;
+	if ( ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U' ){
+	cout << "It is Vowel";
+	}
+	else{
+	cout << "It is Consonant ";
+	}
+	cout << endl;
+	return 0;
+}
