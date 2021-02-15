fddf5d398c7829bca9cf878e054d916599b5097a
i-adarsh <i.adarsh.kr@icloud.com>
2021-02-15 07:16:55 +0530

    Uploaded from bash

---
+++ C++/Basics/ASCII.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout << "Enter a Character : ";
+	cin >> ch;
+	cout << "ASCII of given Character : " << int(ch);
+	cout << endl;
+	return 0;
+}
+
+
