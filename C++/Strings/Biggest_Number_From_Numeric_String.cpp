e680946feceb3afc5cba1a2cdad75e2ede37573d
i-adarsh <i.adarsh.kr@icloud.com>
2021-03-02 11:03:09 +0530

    Uploaded From Bash

--- C++/Strings/Biggest_Number_From_Numeric_String.cpp
+++ C++/Strings/Biggest_Number_From_Numeric_String.cpp
@@ -2,9 +2,12 @@
 #include<string>
 #include<algorithm>
 
-int main (){
+using namespace std;
+int main()
+{
 
-    
-
+    string s = "65392817194094";
+    sort(s.begin(), s.end(), greater<int>());
+    cout << s << endl;
     return 0;
 }
