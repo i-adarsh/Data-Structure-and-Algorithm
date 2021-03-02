74001d7756e47cd027c7e3200c5617a667928adc
i-adarsh <i.adarsh.kr@icloud.com>
2021-03-02 11:43:48 +0530

    Uploaded From Bash

--- C++/Strings/Maximum_Character_Occur.cpp
+++ C++/Strings/Maximum_Character_Occur.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 int main(){
 
-    
+    string str = "sahgyuewbghsfsbcdfuyw";
 
+    char freq[26];
+
+    for (int i = 0; i < 26; i++)
+        freq[i] = 0;
+    for (int i = 0; i <str.size(); i++)
+        freq[str[i] - 'a']++;
+
+    char ans = 'a';
+    int maxF = 0;
+
+    
     return 0;
 }
