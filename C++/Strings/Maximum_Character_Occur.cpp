4acab793b4d064af25da97574ffa1e94a37c7e6b
i-adarsh <i.adarsh.kr@icloud.com>
2021-03-02 11:48:44 +0530

    Uploaded From Bash

--- C++/Strings/Maximum_Character_Occur.cpp
+++ C++/Strings/Maximum_Character_Occur.cpp
@@ -5,18 +5,25 @@
 using namespace std;
 int main(){
 
-    string str = "sahgyuewbghsfsbcdfuyw";
+    string str = "sahgyuewbdfytdhjgiuutyyrtdfgfdgyrryrghjgghsfsbcdfuyw";
 
     char freq[26];
 
     for (int i = 0; i < 26; i++)
         freq[i] = 0;
-    for (int i = 0; i <str.size(); i++)
+    for (int i = 0; i < str.size(); i++)
         freq[str[i] - 'a']++;
 
     char ans = 'a';
     int maxF = 0;
 
-    
+    for (int i = 0; i < 26; i++){
+        if (freq[i] >= maxF){
+            maxF = freq[i];
+            ans = i + 'a';
+        }
+    }
+    cout << maxF << endl;
+    cout << ans << endl;
     return 0;
 }
