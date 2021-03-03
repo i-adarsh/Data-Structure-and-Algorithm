b8ca2766fe5fa190e6b22163b64843cd15f35209
i-adarsh <i.adarsh.kr@icloud.com>
2021-03-03 11:39:22 +0530

    Uploaded from Terminal

--- C++/Recursion/factorial.cpp
+++ C++/Recursion/factorial.cpp
@@ -5,21 +5,18 @@ using namespace std;
 int factorial(int n){
 
   if (n == 0){
-     return 1;
+    return 1;
   }
-  int prevFactorial = factorial(n - 1);
-
-  return n * prevFactorial;
-
+  // int prevFactorial = factorial(n - 1);
+  // return n * prevFactorial;
+  return n * factorial(n - 1);
 }
 
 int main (){
 
   int n;
   cin >> n;
-
   cout << factorial(n) << endl;
-
   return 0;
 
 }
