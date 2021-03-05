9b16320e36e822938e8dbd843b2cee8be9346cc6
i-adarsh <i.adarsh.kr@icloud.com>
2021-03-05 07:56:08 +0530

    Uploaded from bash

--- C++/Recursion/first_last_occurence.cpp
+++ C++/Recursion/first_last_occurence.cpp
@@ -16,7 +16,7 @@ int firstOcc(int arr[], int n, int i, int key){
 }
 
 int main () {
-  int arr[] = {4,2,5,6,7,8,3,5};
+  int arr[] = {1,4,2,5,6,7,8,3,5};
   int key = 0;
   cin >> key;
   cout << firstOcc(arr, 8, 0, key) << endl;
