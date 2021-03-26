905bd3a8eb8f36b45c1610b2fcd7573129c500c0
i-adarsh <i.adarsh.kr@icloud.com>
2021-03-26 07:25:44 +0530

    Uploaded from bash

---
+++ C++/Recursion/reverse_String.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+void reverse(string s){
+
+  if (s.length()==0){
+    return;
+ }
+
+  string rest = s.substr(1);
+  reverse(rest);
+  cout << s[0];
+
+}
+
+int main () {
+  reverse("Adarsh");
+  cout << endl;
+  return 0;
+}
+
