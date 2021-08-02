fee80a4f96d4273d864f4abdd6aaf1d921f0b07b
i-adarsh <i.adarsh.kr@icloud.com>
2021-08-02 07:54:05 +0530

    Uploaded from bash

--- C++/Stack/postfix_Evaluation.cpp
+++ C++/Stack/postfix_Evaluation.cpp
@@ -1,1 +1,47 @@
-#include<>
+#include<iostream>
+#include<stack>
+#include<math.h>
+
+using namespace std;
+
+int postfixEvaluation (string s){
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++){
+        if(s[i] >= '0' && s[i] <= '9'){
+            st.push(s[i] - '0');
+        }
+        else{
+            int operand1 = st.top();
+            st.pop();
+            int operand2 = st.top();
+            st.pop();
+
+            switch (s[i])
+            {
+            case '+':
+                st.push(operand1 + operand2);
+                break;
+            case '-':
+                st.push(operand1 - operand2);
+                break;
+            case '*':
+                st.push(operand1 * operand2);
+                break;
+            case '/':
+                st.push(operand1 / operand2);
+                break;
+            case '^':
+                st.push(pow(operand1, operand2));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+
+int main(){
+    string s = "46+2/5*7+";
+    cout << postfixEvaluation(s) << endl;
+}
