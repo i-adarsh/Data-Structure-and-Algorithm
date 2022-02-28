f090c73b57db0958ab3be53cbccc15d0ebd27696
i-adarsh <i.adarsh.kr@icloud.com>
2022-02-28 14:48:07 +0530

    Uploaded from bash

--- Java/Scaler/BitManipulation/OddEven.java
+++ Java/Scaler/BitManipulation/OddEven.java
@@ -1,6 +1,12 @@
 public class OddEven{
     public static void main(String [] args){
         int [] arr = {1,2,3,4,5,6,7,8,9,10,11,12};
+        /*
+            1. If (a & 1 == 0), then a is even and its LSB is 0.
+            2. If (a & 1 == 1), then a is odd and its LSB is 1.
+            3. If (a | 1 == a + 1), then a is even.
+            4. If (a | 1 == a), then a is odd.
+        */
         for (int a : arr){
             if ((a & 1) == 0){
                 System.out.print("Even ");
