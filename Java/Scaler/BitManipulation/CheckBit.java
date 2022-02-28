480ac4e9df0161556b142397127acfa3d5c9462c
i-adarsh <i.adarsh.kr@icloud.com>
2022-02-28 22:23:34 +0530

    Uploaded from bash

--- Java/Scaler/BitManipulation/CheckBit.java
+++ Java/Scaler/BitManipulation/CheckBit.java
@@ -12,14 +12,18 @@ public class CheckBit{
         return b;
     }
 
-    public static boolean checkBit(int n, int pos){
+    public static boolean checkBitRightShift(int n, int pos){
         return (((n>>pos)&1) == 1);
     }
 
+    public static boolean checkBitLeftShift(int n, int pos){
+        return ((n&(1<<pos)) != 0);
+    }
+
     public static void main(String[] args){
-        System.out.println(checkBit(25,3));
-        System.out.println(checkBit(25,2));
-        System.out.println(checkBit(24,2));
-        System.out.println(checkBit(14,3));
+        System.out.println(checkBitLeftShift(25,3));
+        System.out.println(checkBitLeftShift(25,2));
+        System.out.println(checkBitLeftShift(24,2));
+        System.out.println(checkBitLeftShift(14,3));
     }
 }
