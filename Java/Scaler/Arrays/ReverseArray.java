6a46d0c825d622dbb2f4468883ad032f4311b0eb
i-adarsh <i.adarsh.kr@icloud.com>
2022-03-04 12:35:10 +0530

    Uploaded from bash

--- Java/Scaler/Arrays/ReverseArray.java
+++ Java/Scaler/Arrays/ReverseArray.java
@@ -4,14 +4,25 @@ public class ReverseArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         int [] arr = {3,4,5,1,2,6};
-        for(int i = 0; i < arr.length/2; i++){
-            int temp = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1- i] = temp;
-        }
+        // for(int i = 0; i < arr.length/2; i++){
+        //     int temp = arr[i];
+        //     arr[i] = arr[arr.length - 1 - i];
+        //     arr[arr.length - 1- i] = temp;
+        // }
+        reverse(arr, 2, arr.length);
         for (int a : arr){
             System.out.print(a + " ");
         }
 	}
 
+    public static void reverse(int [] arr, int start, int end){
+        while(start < (end - 1)){
+            int temp = arr[start];
+            arr[start] = arr[end - 1];
+            arr[end - 1] = temp;
+            
+            start++;
+            end--;
+        }
+    }
 }
